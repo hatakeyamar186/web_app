21f73f0dbba91cb8c1dfacce926952d2422a62c9
Hatakeyama Ryuku <b1022186@fun.ac.jp>
2025-02-18 22:59:57 +0900

    todoアプリのたたき台を作った

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-'use strict';
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.getElementById("todo-input");
+    const addButton =document.getElementById("add-btn");
+    const todoList = document.getElementById("todo-list");
 
-const bmi = 27;
-console.log(`あなたのBMIは${bmi}です`);
+    addButton.addEventListener("click", () => {
+        const task = input.value.trim();
+        todoList.textContent = task;
+        todoList.addEventListener("click", () => {
+            todoList.remove();
+        });
+
+        todoList.appendChild(li);
+        input.value = "";
+    })
+})
